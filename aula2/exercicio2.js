b589748e82b5ecf87e6c8051ab8e77201ca17338
Lucas Honorato Pereira <honoratopereiralucas99@gmail.com>
2026-03-02 08:15:54 -0300

    Add files via upload

---
+++ aula2/exercicio2.js
@@ -0,0 +1,8 @@
+//Exercicio2
+
+var n1 = 2;
+var n2 = 5;
+var n3 = 3;
+
+var nf= n1 + n2 + n3 / 10;
+console.log(nf)
---
+++ aula2/script.js
@@ -0,0 +1,52 @@
+// Exercicio 1 
+
+//Letra A
+var a = 10
+var b = 20
+
+console.log(b);
+
+b=5;
+
+console.log(a,b)
+
+//Letra B
+
+var a = 30;
+var b = 20;
+var c = a + b;
+
+console.log(c);
+
+b = 10;
+
+console.log(b, c); 
+
+var c = a + b;
+
+console.log(a, b, c); 
+
+//Letra C
+
+var a = 10;
+var b = 20;
+var c = a;
+var b = c;
+var a = b;
+
+console.log(a, b, c);
+
+
+//Letra D
+
+var a = 10;
+var b = a + 1;
+var a = b + 1;
+var b = a + 1;
+
+console.log(a);
+
+var a = b + 1;
+
+console.log(a, b);
+
